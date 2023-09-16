15ff167a6b8aa474ab36f8a9f5ee81b47e668e73
Mithuncy <58121761+Mithuncy01@users.noreply.github.com>
2023-09-16 20:35:32 +0600

    2

--- problemSolving.cpp
+++ problemSolving.cpp
@@ -29,3 +29,43 @@ int main() {
 
     return 0;
 }
+
+
+//hourglassSum
+#include <iostream>
+#include <vector>
+
+int hourglassSum(std::vector<std::vector<int>>& arr) {
+    int maxSum = -INT_MAX;
+
+    for (int i = 0; i <= 3; i++) {
+        for (int j = 0; j <= 3; j++) {
+            int currentSum = 
+                arr[i][j] + arr[i][j+1] + arr[i][j+2] +
+                            arr[i+1][j+1] +
+                arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+        }
+    }
+
+    return maxSum;
+}
+
+int main() {
+    std::vector<std::vector<int>> arr = {
+        {1, 1, 1, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0},
+        {1, 1, 1, 0, 0, 0},
+        {0, 0, 2, 4, 4, 0},
+        {0, 0, 0, 2, 0, 0},
+        {0, 0, 1, 2, 4, 0}
+    };
+
+    int maxHourglassSum = hourglassSum(arr);
+    std::cout << maxHourglassSum << std::endl; // Output: 19
+
+    return 0;
+}
